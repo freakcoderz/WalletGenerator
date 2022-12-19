4b6311829abce35106148dc8f6e0c3108760f7b7
Freak House <thehouseoffreak@gmail.com>
2022-12-19 08:47:00 -0500

    Remove dead coins

--- src/janin.currency.js
+++ src/janin.currency.js
@@ -12,28 +12,28 @@ janin.currency = {
         return currency;
     },
 
-    name: function() {
+    name: function () {
         return janin.selectedCurrency.name;
     },
 
-    networkVersion: function() {
+    networkVersion: function () {
         return janin.selectedCurrency.networkVersion;
     },
 
-    privateKeyPrefix: function() {
+    privateKeyPrefix: function () {
         return janin.selectedCurrency.privateKeyPrefix;
     },
 
-    WIF_RegEx: function() {
+    WIF_RegEx: function () {
         return new RegExp("^" + janin.selectedCurrency.WIF_Start + "[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{50}$");
     },
 
-    CWIF_RegEx: function() {
+    CWIF_RegEx: function () {
         return new RegExp("^" + janin.selectedCurrency.CWIF_Start + "[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{51}$");
     },
 
     // Switch currency
-    useCurrency: function(index) {
+    useCurrency: function (index) {
         janin.selectedCurrency = janin.currencies[index];
 
         var coinImgUrl = "logos/" + janin.currency.name().toLowerCase() + ".png";
@@ -57,8 +57,7 @@ janin.currency = {
         document.getElementById("culturepl").href = "?culture=pl&currency=" + janin.currency.name().toLowerCase();
         document.getElementById("culturezh").href = "?culture=zh&currency=" + janin.currency.name().toLowerCase();
 
-        if(ninja.seeder.isDone())
-        {
+        if (ninja.seeder.isDone()) {
             // Regenerate a new wallet when not expensive
             ninja.wallets.singlewallet.generateNewAddressAndKey();
             ninja.wallets.paperwallet.build(document.getElementById('paperpassphrase').value);
@@ -74,14 +73,7 @@ janin.currency = {
 
 janin.currencies = [
     //                              name, 		networkVersion, privateKeyPrefix, WIF_Start, CWIF_Start, donate
-    janin.currency.createCurrency ("Aevo",                0x17, 0x99, "6",    "P"    , ""),
-	janin.currency.createCurrency ("Bitcoin",             0x00, 0x80, "5",    "[LK]" , ""),
-	janin.currency.createCurrency ("DRV",                 0x1e, 0x31, "2",    "8"    , ""),
-	janin.currency.createCurrency ("DRVF",                0x1f, 0xc1, "7",    "V"    , ""),
-	janin.currency.createCurrency ("Empower",                0x21, 0xc7, "7",    "W"    , ""),
-	janin.currency.createCurrency ("freakchain",         0x23, 0xa3, "6",    "R"    , ""),
-	janin.currency.createCurrency ("MentX",		           0x4b, 0xcb, "7", "X"    , ""),
-	janin.currency.createCurrency ("Omen",                 0x35, 0xb5, "7",    "T"    , ""),
-	janin.currency.createCurrency ("SHMN",		      	  0x32, 0xd4, "8",    "Y"    , ""),
-	janin.currency.createCurrency ("Stronghands",		  0x3f, 0xbf, "7",    "V"    , ""),
-                   ];
+    janin.currency.createCurrency("Bitcoin", 0x00, 0x80, "5", "[LK]", ""),
+    janin.currency.createCurrency("Empower", 0x21, 0xc7, "7", "W", ""),
+    janin.currency.createCurrency("freakchain", 0x23, 0xa3, "6", "R", ""),
+];
